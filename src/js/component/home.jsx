04420975960276288e8033b837790d5cd3534e38
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2023-07-04 22:38:37 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,23 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex justify-content-center">
+				<Card img={"https://m.media-amazon.com/images/I/71kT2yfNZHL.jpg"}/>
+				<Card img={"https://hips.hearstapps.com/hmg-prod/images/bbyoda-1575303784.jpeg?resize=1200:*"}/>
+				<Card img={"https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-02-16-at-5.26.01-PM.png"}/>
+				<Card img={"https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-02-16-at-5.26.01-PM.png"}/>
+			</div>
+			<Footer />
 		</div>
 	);
 };
